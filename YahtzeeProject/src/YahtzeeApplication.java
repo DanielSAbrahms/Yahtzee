b36ede96949e77edb979adc4d43cf2b566bb0509
Daniel Abrahms <dabrahms@zagmail.gonzaga.edu>
2017-03-13 15:58:13 -0700

    Added GUI [unstable]

---
+++ YahtzeeProject/src/YahtzeeApplication.java
@@ -0,0 +1,12 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class YahtzeeApplication {
+    public static void main (String [] args) {
+        JFrame frame = new JFrame("Yahtzee");
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setSize(1280, 720);
+        frame.setVisible(true);
+
+    }
+}
---
+++ YahtzeeProject/src/YahtzeeGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by DanielA on 3/13/2017.
+ */
+public class YahtzeeGUI {
+
+    private JTable table1;
+    private JButton rollButton;
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JCheckBox checkBox3;
+    private JCheckBox checkBox4;
+    private JCheckBox checkBox5;
+}
